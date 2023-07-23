e520c103f2b751bb5f686ef131e937887a6228d1
sajjadabd <mynameissajjad@gmail.com>
2023-07-23 00:51:38 -0700

    create MenuSeeder

---
+++ database/seeders/MenuSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+use App\Models\Menu;
+
+class MenuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    /*
+    $table->id();
+    $table->integer('key')->nullable();
+    $table->unsignedBigInteger('parent')->nullable();
+    $table->foreign('parent')->references('id')->on('menus')->onDelete('cascade');
+    $table->string('title');
+    $table->string('path')->nullable();
+    $table->timestamps();
+    */
+
+    public function run(): void
+    {
+        //
+        Menu::create([
+            'key' => 1 ,
+            'parent' => null ,
+            'title' => 'ایجاد غلطک' ,
+            'path' => ''
+        ]);
+
+        Menu::create([
+            'key' => 2 ,
+            'parent' => 1 ,
+            'title' => 'سکشن' ,
+            'path' => '/createroll/section'
+        ]);
+
+        Menu::create([
+            'key' => 3 ,
+            'parent' => 1 ,
+            'title' => 'بارمیل' ,
+            'path' => '/createroll/barmill'
+        ]);
+    }
+}
