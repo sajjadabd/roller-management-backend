4f889074159614804f6ad036c5d71f7423dcd666
sajjadabd <mynameissajjad@gmail.com>
2023-08-04 22:04:55 -0700

    add route functions to roller controller

--- app/Http/Controllers/RollerController.php
+++ app/Http/Controllers/RollerController.php
@@ -9,4 +9,71 @@ use \App\Models\Roller;
 class RollerController extends Controller
 {
     //
+
+    public function getAll() {
+        return Roller::all();
+    }
+
+
+    public function delete(Request $request) {
+
+        $validated = $request->validate([
+            'array' => 'required',
+        ]);
+
+        error_log(implode( " , " , $validated['array'] ));
+
+        Roller::destroy($validated['array']);
+
+    }
+
+
+    public function create(Request $request) {
+
+        // rollType ,
+        // rollModel ,
+        // rollCode ,
+        // rollPosition ,
+        // rollGender ,
+        // rollDiameter ,
+        // rollWidth ,
+        // numberOfCalibres ,
+        // calibreWidth ,
+
+        $validated = $request->validate([
+            
+            'rollType' => 'required|max:225',
+            'rollModel' => 'required|max:225',
+            'rollCode' => 'required|max:225',
+            'rollPosition' => 'required|max:225',
+            'rollGender' => 'required|max:225',
+            'rollDiameter' => 'required|max:225',
+            'rollWidth' => 'required|max:225',
+
+            'numberOfCalibres' => 'required|max:225',
+            'calibreWidth' => 'required|max:225',
+
+        ]);
+
+        if(array_key_exists('parent',$validated)) {
+            if($validated['parent'] == 0) {
+                $validated['parent'] = null ;
+            }
+        } 
+
+        $roller = Menu::create([
+            'roller_code' => $validated['rollCode'] ?? null ,
+            'gender' => $validated['rollGender'] ?? null ,
+            'type' => $validated['rollType'] ,
+            'position' => $validated['rollPosition'] ,
+            'diameter' => $validated['rollDiameter'] ,
+            'width' => $validated['rollWidth'] ,
+        ]);
+
+        $roller->key = $roller->id;
+        $roller->save();
+
+        return $roller;
+
+    }
 }
